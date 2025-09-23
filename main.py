98762765cc545ccb6cc0b6231b696f5f36937259
Balder <balder.ansgar@gmail.com>
2025-09-23 16:44:27 -0300

    add função atualizar

--- main.py
+++ main.py
@@ -7,36 +7,45 @@ conexao = sqlite3.connect("escola.db")
 #Criar um objeto "cursor" server para executar os comandos SQL
 cursor = conexao.cursor()
 
-#criar uma tabela no banco de dados
+# #criar uma tabela no banco de dados
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS alunos (
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     nome TEXT NOT NULL,
+#     idade INTEGER,
+#     curso TEXT)
+# """)
+# print("Tabela criada com sucesso!")
+
+# # Inserir um aluno no banco de dados
+# nome = input("Digite o nome do aluno que deseja cadastrar: ")
+# cursor.execute("""
+# INSERT INTO alunos (nome,idade,curso)
+# VALUES (?,?,?)
+# """,(nome, 20, "Medicina"))
+
+# # Podemos cadastrar varias informações no banco de dados
+# alunos = [
+#     ("Enzo", 22, "Direito"),
+#     ("Murilo", 33, "computação"),
+#     ("Eduardo", 41, "Computação")
+# ]
+
+# cursor.executemany("""
+# INSERT INTO alunos (nome,idade,curso)
+# VALUES (?,?,?)
+# """,(alunos)
+# )
+
+# # Precisamos confirmar as alterações no banco de dados
+# conexao.commit()
+
+# Atualizar os dados no banco de dados
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS alunos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    idade INTEGER,
-    curso TEXT)
-""")
-print("Tabela criada com sucesso!")
-
-# Inserir um aluno no banco de dados
-nome = input("Digite o nome do aluno que deseja cadastrar: ")
-cursor.execute("""
-INSERT INTO alunos (nome,idade,curso)
-VALUES (?,?,?)
-""",(nome, 20, "Medicina"))
-
-# Podemos cadastrar varias informações no banco de dados
-alunos = [
-    ("Enzo", 22, "Direito"),
-    ("Murilo", 33, "computação"),
-    ("Eduardo", 41, "Computação")
-]
-
-cursor.executemany("""
-INSERT INTO alunos (nome,idade,curso)
-VALUES (?,?,?)
-""",(alunos)
-)
-
-# Precisamos confirmar as alterações no banco de dados
+UPDATE alunos
+SET nome = ?, idade = ?,curso = ?
+WHERE id = ?
+""",("Maria",18, "ADS", 2))
 conexao.commit()
+print("Dados atualizados com sucesso!")
 
